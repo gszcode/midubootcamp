0c335280bd4ccf38aff816169dc1ffdb8227a6fd
Gabriel <gszcode@gmail.com>
2023-10-31 23:08:52 -0300

    esquema y modelo users creado

---
+++ api/study/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'Note'
+    }
+  ]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+userSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('User', userSchema)
