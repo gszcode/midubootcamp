d23c02bd9910342a888b6eed91149f3823d6c07c
Gabriel <gszcode@gmail.com>
2023-10-31 23:09:31 -0300

    conexion a mongodb

--- api/study/mongo.js
+++ api/study/mongo.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
+const { MONGODB_URI, TEST_MONGODB_URI, NODE_ENV } = process.env
+
+const connectionString = NODE_ENV === 'test' ? TEST_MONGODB_URI : MONGODB_URI
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(connectionString)
   .then((res) => console.log('Todo OK!'))
   .catch((err) => console.log('Errorsito', err))
 
